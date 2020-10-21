8ef38204a06ccf42e69554530060a7a17380b93e
mwntech-edu <69295988+mwntech-edu@users.noreply.github.com>
2020-10-21 15:44:56 +0900

    ScriptableObject example

---
+++ Assets/MyZombie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyZombie : MonoBehaviour
+{
+    public ZombieData m_Data;
+
+    public int m_Hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Hp = m_Data.Hp;
+    }
+
+    private void OnDestroy()
+    {
+        m_Data.Hp = m_Hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ZombieData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "ZombieDB", menuName = "Shader/Zombie Data", order = int.MaxValue)]
+public class ZombieData : ScriptableObject
+{
+    [SerializeField]
+    private string zombieName;
+    public string ZombieName { get { return zombieName; } }
+
+    [SerializeField]
+    private int hp;
+
+    public int Hp { get { return hp; } set { hp = value; } }
+
+    [SerializeField]
+
+    private int damage;
+
+    public int Damage { get { return damage; } }
+
+    [SerializeField]
+
+    private float sightRange;
+
+    public float SightRange { get { return sightRange; } }
+
+    [SerializeField]
+
+    private float moveSpeed;
+
+    public float MoveSpeed { get { return moveSpeed; } }
+
+}
