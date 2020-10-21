f095e1f25007406a308c53356eaf3776a9a22276
HeartKo <69335537+HeartKo@users.noreply.github.com>
2020-10-21 15:48:08 +0900

    222

---
+++ Assets/ZombieData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "Zombie Data", menuName = "Scriptable Object/Zombie Data", order = int.MaxValue)]
+
+public class ZombieData : ScriptableObject
+
+{
+
+    [SerializeField]
+
+    private string zombieName;
+
+    public string ZombieName { get { return zombieName; } }
+
+    [SerializeField]
+
+    private int hp;
+
+    public int Hp { get { return hp; } }
+
+    [SerializeField]
+
+    private int damage;
+
+    public int Damage { get { return damage; } }
+
+    [SerializeField]
+
+    private float sightRange;
+
+    public float SightRange { get { return sightRange; } }
+
+    [SerializeField]
+
+    private float moveSpeed;
+
+    public float MoveSpeed { get { return moveSpeed; } }
+
+}
